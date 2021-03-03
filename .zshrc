6e1a0c6a5fb7645cd493d026bc29695aa4e1ec24
Salvador Lopez Mendoza <sallomendo@gmail.com>
2021-03-03 15:15:33 -0600

    Initial commit

---
+++ .zshrc
@@ -0,0 +1,21 @@
+export NVM_AUTO_USE=true
+
+# Antigen
+source /usr/local/share/antigen/antigen.zsh
+
+antigen use oh-my-zsh
+
+antigen bundle git
+antigen bundle lukechilds/zsh-nvm
+antigen bundle mattberther/zsh-rbenv
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+antigen theme miloshadzic
+antigen apply
+
+# React Native
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+export PATH=$PATH:$ANDROID_HOME/emulator
---
+++ setup.sh
@@ -0,0 +1,28 @@
+brew install zsh
+brew install antigen
+brew install yarn
+
+brew install --cask iterm2
+brew install --cask hammerspoon 
+brew install --cask visual-studio-code
+brew install --cask docker
+
+brew install --cask flux
+brew install --cask numi
+brew install --cask postico
+brew install --cask proxyman
+brew install --cask raycast
+brew install --cask slack
+brew install --cask spotify
+brew install --cask transmission
+brew install --cask vlc
+
+brew install --cask zoom
+brew install --cask sublime-text
+brew install --cask sketch
+brew install --cask figma
+
+ln -sf ~/Code/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/Code/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/Code/dotfiles/.ssh ~/.ssh
+ln -sf ~/Code/dotfiles/.hammerspoon ~/.hammerspoon
