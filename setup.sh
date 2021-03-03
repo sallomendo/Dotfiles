78132f8515a6d58140c496a56081ae4747c5b07c
Salvador Lopez Mendoza <sallomendo@gmail.com>
2021-03-03 15:17:48 -0600

    Remove ssh symlink

--- setup.sh
+++ setup.sh
@@ -3,10 +3,9 @@ brew install antigen
 brew install yarn
 
 brew install --cask iterm2
-brew install --cask hammerspoon 
+brew install --cask hammerspoon
 brew install --cask visual-studio-code
 brew install --cask docker
-
 brew install --cask flux
 brew install --cask numi
 brew install --cask postico
@@ -15,14 +14,12 @@ brew install --cask raycast
 brew install --cask slack
 brew install --cask spotify
 brew install --cask transmission
-brew install --cask vlc
-
 brew install --cask zoom
 brew install --cask sublime-text
 brew install --cask sketch
 brew install --cask figma
+brew install --cask vlc
 
 ln -sf ~/Code/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/Code/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/Code/dotfiles/.ssh ~/.ssh
 ln -sf ~/Code/dotfiles/.hammerspoon ~/.hammerspoon
